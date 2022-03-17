5e8c9cb41cab746d4e489568c9a5a3defacb80af
Ronak Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2022-03-17 22:52:06 +0530

    initial commit

---
+++ wp-cli-rest-command/wp-cli-rest-command.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Cli Rest Command
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-cli-rest-command
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Cli_Rest_Command
+ */
+
+// Your code starts here.
